cf96df84f2a0ec962d4fcfd7d951a80ffae60e38
Lautaro silva <80924066+Lauti-Silva@users.noreply.github.com>
2021-06-11 23:32:05 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 //TODO: Requerimos de (hbs);
 app.set('view engine', 'hbs');
